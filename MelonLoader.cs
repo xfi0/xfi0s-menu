1d3f3e4013b93ed017ae3308b05902bd3c3d8d15
dom <166765848+xfi0@users.noreply.github.com>
2024-08-29 18:41:04 -0500

    Add files via upload

---
+++ MelonLoader.cs
@@ -0,0 +1,5 @@
+// MelonLoader, Version=0.5.7.0, Culture=neutral, PublicKeyToken=null
+	// Assembly reference loading information:
+	// There were some problems during assembly reference load, see below for more information!
+	// Error: Could not find reference: MelonLoader, Version=0.5.7.0, Culture=neutral, PublicKeyToken=null
+
