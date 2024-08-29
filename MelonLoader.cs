d193fe25d4b95b5eead34b04e8fbd1842dc3fb4f
dom <166765848+xfi0@users.noreply.github.com>
2024-08-29 18:45:04 -0500

    Update MelonLoader.cs

--- MelonLoader.cs
+++ MelonLoader.cs
@@ -1,5 +1,1 @@
-// MelonLoader, Version=0.5.7.0, Culture=neutral, PublicKeyToken=null
-	// Assembly reference loading information:
-	// There were some problems during assembly reference load, see below for more information!
-	// Error: Could not find reference: MelonLoader, Version=0.5.7.0, Culture=neutral, PublicKeyToken=null
 
